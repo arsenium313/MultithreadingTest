1fad0c0cb5e956f7d11f3dc33bbadb12653b3892
arsenium313 <arsenium313@gmail.com>
2023-04-28 15:18:03 +0300

    Configure SimpleUI

---
+++ ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  MultithreadingTest
+//
+//  Created by Арсений Кухарев on 28.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //MARK: Properties
+    private var imageView: UIImageView!
+    private var getImageButton: UIButton!
+    
+    private lazy var guide = self.view.layoutMarginsGuide
+    
+    
+    //MARK: - View Life Circle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+
+    
+    //MARK: - SetupUI
+    private func setupUI() {
+        configureSelf()
+        configureImageView()
+        configureGetImageButton()
+    }
+
+    private func configureSelf() {
+        self.view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    }
+    
+    private func configureImageView(){
+        imageView = UIImageView()
+        self.view.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: guide.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: guide.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: guide.trailingAnchor),
+            imageView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.7)
+        ])
+        
+        imageView.backgroundColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+    }
+    
+    private func configureGetImageButton() {
+        getImageButton = UIButton(configuration: .filled())
+        getImageButton.configuration?.baseBackgroundColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+        getImageButton.configuration?.title = "Get Image"
+        getImageButton.configuration?.baseForegroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        
+        self.view.addSubview(getImageButton)
+        getImageButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            getImageButton.centerXAnchor.constraint(equalTo: guide.centerXAnchor),
+            getImageButton.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 30)
+        ])
+    }
+}
+
